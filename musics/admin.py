98bb0d6fbb66efe5babc83768a16fa9329380cce
Seong-hwan Hwang <gardendeadman@gmail.com>
2018-03-29 16:37:44 +0900

    어드민 페이지 UI 개선(필터 적용)

--- musics/admin.py
+++ musics/admin.py
@@ -7,12 +7,14 @@ class AlbumAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'musician', 'is_public', 'updated_at']
     list_display_links = ['title']
     list_editable = ['is_public']
+    list_filter = ['musician']
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
     list_display = ['id', 'album', 'order', 'title', 'musician', 'updated_at']
     list_display_links = ['title']
     list_editable = ['album', 'order']
+    list_filter = ['album', 'musician']
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
