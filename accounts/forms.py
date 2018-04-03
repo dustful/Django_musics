4e65d3f2a2f1c5ee7df464bac9aa75121eb818a9
Seong-hwan Hwang <dustful@naver.com>
2018-04-03 23:33:01 +0900

    update admin.py, forms.py, models.py at accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -12,3 +14,13 @@ class SignupForm(UserCreationForm):
             if get_user_model().objects.filter(email = email).exists():
                 raise forms.ValidationError('존재하는 이메일 주소입니다.')
         return email
+
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'location', 'birth_at')
--- musics/models.py
+++ musics/models.py
@@ -3,10 +3,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete = models.CASCADE)
-#     birth_date = models.DateField(null = True, blank = True)
-    
 class Musician(models.Model):
     name = models.CharField(max_length = 100)
     debut_at = models.DateField(null = True, blank = True)
